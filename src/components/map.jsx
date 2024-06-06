7ae9989fab08532d86e72e84530950471bcd63e2
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-06 07:50:36 +0300

    ✨ feat(app): rendering map from user data

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,7 +1,51 @@
-// import React from 'react'
+import { useEffect, useRef, useReducer } from 'react';
 
-const Map = () => {
-  return <div>map</div>;
+// Leaflet imports
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+
+// Variables
+import iconImg from '../assets/icon-location.svg';
+const markerIcon = new Icon({ iconUrl: iconImg, iconSize: [50, 60] });
+
+// Reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'setCoords':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const Map = ({ userCoords }) => {
+  const [coords, dispatch] = useReducer(reducer, [0, 0]);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!userCoords) return;
+
+    dispatch({ type: 'setCoords', payload: userCoords });
+
+    mapRef?.current?.flyTo(userCoords, 16, {
+      duration: 2,
+    });
+  }, [userCoords]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={coords}
+      zoom={13}
+      style={{ minHeight: '80vh', width: '100vw' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={coords} icon={markerIcon}></Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
